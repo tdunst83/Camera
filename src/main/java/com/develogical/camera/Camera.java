6d3fa6df4004c7c47510801d3996176170520f46
tdunst83 <thomas.dunst@avalanchez.de>
2019-03-19 16:51:38 +0000

    Mock done

--- src/main/java/com/develogical/camera/Camera.java
+++ src/main/java/com/develogical/camera/Camera.java
@@ -2,16 +2,54 @@ package com.develogical.camera;
 
 public class Camera {
 
+    Sensor sensor;
+    MemoryCard memCard;
+    boolean status_on;
+    boolean writing;
+
+    public Camera(Sensor sensor) {
+        this.sensor = sensor;
+        this.status_on = false;
+        this.writing = false;
+    }
+
+    public Camera(Sensor sensor, MemoryCard memCard) {
+        this.sensor = sensor;
+        this.memCard = memCard;
+        this.status_on = false;
+        this.writing = false;
+    }
+
     public void pressShutter() {
-        // not implemented
+
+        if(this.status_on) {
+            this.writing = true;
+            byte[] data = this.sensor.readData();
+
+            this.memCard.write(data, new WriteCompleteListener() {
+
+                @Override
+                public void writeComplete() {
+                    Camera.this.writing = false;
+                    Camera.this.powerOff();
+
+                }
+            });
+
+        }
+
     }
 
     public void powerOn() {
-        // not implemented
+        sensor.powerUp();
+        this.status_on=true;
     }
 
     public void powerOff() {
-       // not implemented
+        if (!this.writing) {
+            sensor.powerDown();
+        }
+       this.status_on=false;
     }
 }
 
--- src/test/java/com/develogical/camera/CameraTest.java
+++ src/test/java/com/develogical/camera/CameraTest.java
@@ -1,13 +1,99 @@
 package com.develogical.camera;
 
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
+import org.mockito.*;
 
 import static org.mockito.Mockito.*;
 
 public class CameraTest {
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
-        // write your test here
+
+        Sensor sensor = mock(Sensor.class);
+
+        Camera camera = new Camera(sensor);
+
+        camera.powerOn();
+        verify(sensor).powerUp();
     }
+
+    @Test
+    public void switchingTheCameraOffPowersDownTheSensor() {
+        Sensor sensor = mock(Sensor.class);
+
+        Camera camera = new Camera(sensor);
+
+        camera.powerOff();
+        verify(sensor).powerDown();
+    }
+
+
+    @Test
+    public void pressingShutterCopiesDataFromSensorToMemoryWhenPowersOn(){
+
+        Sensor sensor = mock(Sensor.class);
+        MemoryCard memCard = mock(MemoryCard.class);
+
+        Camera camera = new Camera(sensor, memCard);
+
+        camera.powerOn();
+        camera.pressShutter();
+
+        verify(memCard).write(eq(sensor.readData()), any(WriteCompleteListener.class));
+
+    }
+
+    @Test
+    public void pressingShutterCopiesDataFromSensorToMemoryWhenPowersOff(){
+
+        Sensor sensor = mock(Sensor.class);
+        MemoryCard memCard = mock(MemoryCard.class);
+
+        Camera camera = new Camera(sensor, memCard);
+
+        camera.powerOff();
+        camera.pressShutter();
+        verify(memCard, never()).write(any(), any());
+    }
+
+    @Test
+    public void dataCurrentlyWrittenNoPowerDownBySwitchingOffTheCamera(){
+
+        Sensor sensor = mock(Sensor.class);
+        MemoryCard memCard = mock(MemoryCard.class);
+
+        Camera camera = new Camera(sensor, memCard);
+
+
+        camera.powerOn();
+        camera.pressShutter();
+        camera.powerOff();
+
+        verify(sensor).powerUp();
+        verify(sensor, never()).powerDown();
+    }
+
+    @Test
+    public void onceWritingDataCompleteTheCameraPowersDownTheSensor(){
+
+        Sensor sensor = mock(Sensor.class);
+        MemoryCard memCard = mock(MemoryCard.class);
+
+        ArgumentCaptor<WriteCompleteListener> argument = ArgumentCaptor.forClass(WriteCompleteListener.class);
+
+        Camera camera = new Camera(sensor, memCard);
+
+        camera.powerOn();
+        camera.pressShutter();
+
+        verify(memCard).write(eq(sensor.readData()), argument.capture());
+        verify(sensor, never()).powerDown();
+
+        argument.getValue().writeComplete();
+
+        verify(sensor).powerDown();
+    }
+
+
+
 }
